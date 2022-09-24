493ebcd1acecb8f8adc5fe2d25ffd38baceefb03
kursor <skurochkin298@gmail.com>
2022-09-24 18:54:25 +0300

    finished first screen

--- app/src/main/java/com/kursor/crypto/ui/screens/cryptoList/CryptoCurrencyInfoListScreen.kt
+++ app/src/main/java/com/kursor/crypto/ui/screens/cryptoList/CryptoCurrencyInfoListScreen.kt
@@ -4,18 +4,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.kursor.crypto.R
 import com.kursor.crypto.model.entities.CryptoCurrencyInfo
+import com.kursor.crypto.ui.screens.elements.ChipGroup
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -31,15 +35,28 @@ fun CryptoCurrencyInfoListScreen(
         topBar = {
             TopAppBar() {
                 Column() {
-                    Text(text = stringResource(id = R.string.crypto_currency_list))
+                    Text(
+                        text = stringResource(id = R.string.crypto_currency_list),
+                        modifier = Modifier.padding(8.dp)
+                    )
+                    ChipGroup(
+                        elements = CryptoCurrencyInfoListViewModel.Currency.values().toList(),
+                        onSelectedChanged = {
+                            viewModel.loadData(it)
+                        },
+                        selected = CryptoCurrencyInfoListViewModel.Currency.USD
+                    )
                 }
 
             }
         }
     ) {
-
+        LazyColumn {
+            items(cryptoCurrencyInfoList.value) {
+                CryptoCurrencyInfoListItem(cryptoCurrencyInfo = it)
+            }
+        }
     }
-
 }
 
 @Composable
